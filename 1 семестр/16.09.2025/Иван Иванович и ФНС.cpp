ddf1e99b4bcdc07f61a14c3a1e6baa23d216ac4a
shukaev-A <shukaevandrew@yandex.ru>
2025-09-16 11:43:42 +0300

    Update Иван Иванович и ФНС.cpp

--- 1 семестр/16.09.2025/Иван Иванович и ФНС.cpp
+++ 1 семестр/16.09.2025/Иван Иванович и ФНС.cpp
@@ -1,1 +1,15 @@
-
+#include <iostream>
+using namespace std;
+int main(){
+    int cnt=1;
+    float x,y;
+    cout<<"X\n";
+    cin>>x;
+    cout<<"Y\n";
+    cin>>y;
+    while (x<=y){
+        x=x*1.1;
+        ++cnt;
+    }
+    cout<<cnt;
+}
