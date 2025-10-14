884b49955f04dd2cc491485668ec7148baf33792
shukaev-A <shukaevandrew@yandex.ru>
2025-10-14 11:17:19 +0300

    Update and rename удаление дубл. в масс to удаление дубл. в масс.cpp

---
+++ 1 семестр/14.10.2025/удаление дубл. в масс.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+
+void removeDuplicates(std::vector<int>& arr) {
+    std::unordered_set<int> seen;
+    std::vector<int> result;
+
+    for (int num : arr) {
+        if (seen.find(num) == seen.end()) {
+            seen.insert(num);
+            result.push_back(num);
+        }
+    }
+
+    arr = result;
+
+    // Вывод обновлённого массива
+    for (int num : arr) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    std::vector<int> data = {1, 2, 2, 3, 4, 4, 5, 1};
+    removeDuplicates(data);
+    return 0;
+}
