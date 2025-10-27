ac47e51a121c578ece9ab6a1b403627c1f576dd8
shukaev-A <shukaevandrew@yandex.ru>
2025-10-27 23:06:27 +0300

    Create 6.cpp

---
+++ 1 семестр/алгоритм/6.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void sieveOfSundaram(int n) {
+    int new_limit = (n - 1) / 2;
+    vector<bool> marked(new_limit + 1, false);
+
+    // Отметить все числа, которые по формуле i + j + 2ij <= new_limit
+    for (int i = 1; i <= new_limit; i++) {
+        for (int j = i; (i + j + 2 * i * j) <= new_limit; j++) {
+            marked[i + j + 2 * i * j] = true;
+        }
+    }
+
+    cout << "Простые числа от 2 до " << n << ":\n";
+    if (n > 2) {
+        cout << 2 << " ";
+    }
+    for (int i = 1; i <= new_limit; i++) {
+        if (!marked[i]) {
+            cout << (2 * i + 1) << " ";
+        }
+    }
+    cout << "\n";
+}
+
+int main() {
+    int n;
+    cout << "Введите верхнюю границу для поиска простых чисел: ";
+    cin >> n;
+
+    if (n < 2) {
+        cout << "Простых чисел нет.\n";
+        return 0;
+    }
+
+    sieveOfSundaram(n);
+    return 0;
+}
