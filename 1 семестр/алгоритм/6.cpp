19e1c9d753483a9178e90c2790f9f6c70c08f8f9
shukaev-A <shukaevandrew@yandex.ru>
2025-10-28 20:16:35 +0300

    Update 6.cpp

--- 1 семестр/алгоритм/6.cpp
+++ 1 семестр/алгоритм/6.cpp
@@ -7,14 +7,13 @@ void sieveOfSundaram(int n) {
     int new_limit = (n - 1) / 2;
     vector<bool> marked(new_limit + 1, false);
 
-    // Отметить все числа, которые по формуле i + j + 2ij <= new_limit
     for (int i = 1; i <= new_limit; i++) {
         for (int j = i; (i + j + 2 * i * j) <= new_limit; j++) {
             marked[i + j + 2 * i * j] = true;
         }
     }
 
-    cout << "Простые числа от 2 до " << n << ":\n";
+    cout << n << ":\n";
     if (n > 2) {
         cout << 2 << " ";
     }
