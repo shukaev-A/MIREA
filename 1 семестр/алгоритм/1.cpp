b42aa593093397bc142813a4790f5831614d5d9d
shukaev-A <shukaevandrew@yandex.ru>
2025-10-28 16:30:16 +0300

    Update 1.cpp

--- 1 семестр/алгоритм/1.cpp
+++ 1 семестр/алгоритм/1.cpp
@@ -14,7 +14,6 @@ int gcd_three_division(int a, int b, int c) {
     return gcd_division(gcd_division(a, b), c);
 }
 
-// Функция для НОД методом вычитания для двух чисел
 int gcd_subtraction(int a, int b) {
     while (a != b) {
         if (a > b)
@@ -33,23 +32,20 @@ int gcd_four_subtraction(int a, int b, int c, int d) {
 
 int main() {
     int count;
-    cout << "Введите количество чисел (3 или 4): ";
     cin >> count;
 
     if (count == 3) {
         int x, y, z;
-        cout << "Введите три числа: ";
         cin >> x >> y >> z;
         int result = gcd_three_division(x, y, z);
         cout << "Наибольший общий делитель (метод деления): " << result << endl;
     } else if (count == 4) {
         int a, b, c, d;
-        cout << "Введите четыре числа: ";
         cin >> a >> b >> c >> d;
         int result = gcd_four_subtraction(a, b, c, d);
         cout << "Наибольший общий делитель (метод вычитания): " << result << endl;
     } else {
-        cout << "Ошибка: нужно ввести либо 3, либо 4 числа." << endl;
+        cout << endl;
     }
     return 0;
 }
