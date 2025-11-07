df2e26b4e8d0242c5c9c76f92ed3f0189ab2e5e1
shukaev-A <shukaevandrew@yandex.ru>
2025-11-07 10:57:20 +0300

    Update 5.cpp

--- 1 семестр/алгоритм/тетрадь 3/5.cpp
+++ 1 семестр/алгоритм/тетрадь 3/5.cpp
@@ -2,20 +2,18 @@
 #include <string>
 #include <algorithm>
 
-// Убирает ведущие нули у числа, представленном строкой
-std::string trimLeadingZeros(const std::string& str) {
+std::string trimLeadingZeros(const std::string &str) {
     size_t pos = str.find_first_not_of('0');
     return (pos == std::string::npos) ? "0" : str.substr(pos);
 }
 
-// Сложение больших чисел (строки)
-std::string add(const std::string& a, const std::string& b) {
+std::string add(const std::string &a, const std::string &b) {
     int n = std::max(a.size(), b.size());
     std::string result(n, '0');
     int carry = 0;
     for (int i = 0; i < n; i++) {
-        int digitA = (int)(a.size() - 1 - i >= 0 ? a[a.size() - 1 - i] - '0' : 0);
-        int digitB = (int)(b.size() - 1 - i >= 0 ? b[b.size() - 1 - i] - '0' : 0);
+        int digitA = i < (int)a.size() ? a[a.size() - 1 - i] - '0' : 0;
+        int digitB = i < (int)b.size() ? b[b.size() - 1 - i] - '0' : 0;
         int sum = digitA + digitB + carry;
         carry = sum / 10;
         result[n - 1 - i] = (sum % 10) + '0';
@@ -24,14 +22,13 @@ std::string add(const std::string& a, const std::string& b) {
     return result;
 }
 
-// Вычитание больших чисел (a >= b)
-std::string subtract(const std::string& a, const std::string& b) {
+std::string subtract(const std::string &a, const std::string &b) { // a >= b
     int n = a.size();
     std::string result = a;
     int carry = 0;
     for (int i = 0; i < n; i++) {
-        int digitA = (int)(a.size() - 1 - i >= 0 ? a[a.size() - 1 - i] - '0' : 0);
-        int digitB = (int)(b.size() - 1 - i >= 0 ? b[b.size() - 1 - i] - '0' : 0);
+        int digitA = a[n - 1 - i] - '0';
+        int digitB = i < (int)b.size() ? b[b.size() - 1 - i] - '0' : 0;
         int diff = digitA - digitB - carry;
         if (diff < 0) {
             diff += 10;
@@ -44,70 +41,63 @@ std::string subtract(const std::string& a, const std::string& b) {
     return trimLeadingZeros(result);
 }
 
-// Умножение простым методом (для базового случая)
-std::string multiplySimple(const std::string& a, const std::string& b) {
+std::string multiplySimple(const std::string &a, const std::string &b) {
     int n = a.size();
     int m = b.size();
     std::string res(n + m, '0');
-    for (int i = n-1; i >= 0; i--) {
+    for (int i = n - 1; i >= 0; i--) {
         int carry = 0;
         int digitA = a[i] - '0';
-        for (int j = m-1; j >= 0; j--) {
+        for (int j = m - 1; j >= 0; j--) {
             int digitB = b[j] - '0';
-            int tmp = (res[i+j+1] - '0') + digitA * digitB + carry;
+            int tmp = (res[i + j + 1] - '0') + digitA * digitB + carry;
             carry = tmp / 10;
-            res[i+j+1] = (tmp % 10) + '0';
+            res[i + j + 1] = (tmp % 10) + '0';
         }
         res[i] += carry;
     }
     return trimLeadingZeros(res);
 }
 
-// Добивает строку слева нулями до длины n
-std::string padLeft(const std::string& str, size_t n) {
+std::string padLeft(const std::string &str, size_t n) {
     if (str.size() >= n) return str;
     return std::string(n - str.size(), '0') + str;
 }
 
-// Метод Карацубы
-std::string karatsuba(const std::string& x, const std::string& y) {
-    // Обработка базовых случаев
+std::string karatsuba(const std::string &x, const std::string &y) {
     int n = std::max(x.size(), y.size());
-    if (n <= 4)  // Можно регулировать порог для простого умножения
+    if (n <= 4) 
         return multiplySimple(x, y);
+    if (x == "0" || y == "0")
+        return "0";
 
-    n = (n / 2) + (n % 2); // Середина
+    int m = (n + 1) / 2;
 
-    // Дополняем строки нулями слева до одинаковой длины 2n
-    std::string X = padLeft(x, 2 * n);
-    std::string Y = padLeft(y, 2 * n);
+    std::string X = padLeft(x, n);
+    std::string Y = padLeft(y, n);
 
-    // Разделяем числа
-    std::string a = X.substr(0, n);
-    std::string b = X.substr(n, n);
-    std::string c = Y.substr(0, n);
-    std::string d = Y.substr(n, n);
+    std::string a = X.substr(0, n - m);
+    std::string b = X.substr(n - m);
+    std::string c = Y.substr(0, n - m);
+    std::string d = Y.substr(n - m);
 
-    // Х = a*10^n + b, Y = c*10^n + d
     std::string ac = karatsuba(a, c);
     std::string bd = karatsuba(b, d);
-    std::string ab_cd = karatsuba(add(a, b), add(c, d));
-    std::string ad_bc = subtract(subtract(ab_cd, ac), bd);
+    std::string ab = add(a, b);
+    std::string cd = add(c, d);
+    std::string abcd = karatsuba(ab, cd);
+    std::string adbc = subtract(subtract(abcd, ac), bd);
 
-    // Результат = ac*10^(2n) + (ad+bc)*10^n + bd
-    std::string result = add(add(ac + std::string(2 * n, '0'), ad_bc + std::string(n, '0')), bd);
+    ac.append(2 * m, '0');
+    adbc.append(m, '0');
+
+    std::string result = add(add(ac, adbc), bd);
     return trimLeadingZeros(result);
 }
 
 int main() {
     std::string num1, num2;
-    std::cout << "Введите первое большое число: ";
-    std::cin >> num1;
-    std::cout << "Введите второе большое число: ";
-    std::cin >> num2;
-
-    std::string product = karatsuba(num1, num2);
-    std::cout << "Произведение (метод Карацубы): " << product << std::endl;
-
+    std::cin >> num1 >> num2;
+    std::cout << karatsuba(num1, num2) << "\n";
     return 0;
 }
