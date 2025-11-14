842c1b539923d6db79b3550a370ff14609bb11cb
shukaev-A <shukaevandrew@yandex.ru>
2025-11-14 09:45:31 +0300

    Update 1.cpp

--- 1 семестр/алгоритм/тетрадь 4/1.cpp
+++ 1 семестр/алгоритм/тетрадь 4/1.cpp
@@ -6,7 +6,6 @@ void bubbleSort(int arr[], int n) {
         bool swapped = false; 
         for (int j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
-                // меняем элементы местами
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
