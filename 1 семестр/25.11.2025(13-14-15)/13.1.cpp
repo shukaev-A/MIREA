48777c342a64649c021888dea9dda4dd543f7859
shukaev-A <shukaevandrew@yandex.ru>
2025-12-04 11:07:37 +0300

    Update 13.1.cpp

--- 1 семестр/25.11.2025(13-14-15)/13.1.cpp
+++ 1 семестр/25.11.2025(13-14-15)/13.1.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 
+int main() {
+    std::vector<int> common(26, 1); // флаг для каждой буквы: 1 - может быть везде
+    std::string word;
+
+    bool firstWord = true;
+    while (std::getline(std::cin, word)) {
+        std::vector<int> present(26, 0);
+        for (char c : word) {
+            present[c - 'a'] = 1;
+        }
+
+        if (firstWord) {
+            common = present;  // инициализация для первого слова
+            firstWord = false;
+        } else {
+            for (int i = 0; i < 26; ++i) {
+                common[i] = common[i] && present[i]; // пересечение множества букв
+            }
+        }
+    }
+
+    for (int i = 0; i < 26; ++i) {
+        if (common[i]) {
+            std::cout << char('a' + i);
+        }
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
